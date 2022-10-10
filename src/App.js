31f4e3ea86ed7cc2e27c79a9ba0225ef011945c8
Michelle <micgep1015@gmail.com>
2022-10-09 19:34:48 -0700

    computer animation and single page application capability

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+// We will create these two pages in a moment
+import Services from './Services';
+import Projects from './Projects';
+import Contact from './Contact';
+import Home from './Home';
+
+
+function App() {
+  return (
+    
+    <Routes>
+   
+        <Route path="/" element={<Home/>}>
+        {/*<Route index element={<Home />} />*/}
+        <Route path="/services" element={<Services/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Route>
+    
+    </Routes>
+    
+     
+  
+  );
+}
+
+export default App;
